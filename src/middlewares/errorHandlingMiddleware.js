f73d5391d0aaa25cdd25149501eb181fbeab50fc
Lê Quý Độ <leedo.devwork@gmail.com>
2024-06-01 15:21:38 +0700

    Middleware: errorHandler

---
+++ src/middlewares/errorHandlingMiddleware.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { StatusCodes } from 'http-status-codes'
+// import { env } from '~/config/environment'
+
+// Centralized Error Handleing Middleware in Back-end NodeJS App
+export const errorHandlingMiddleware = (err, req, res, next) => {
+  // Default statusCode 500 INTERNAL_SERVER_ERROR if doesn't exist
+  if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+
+  // Create responseError to control whatever wanted to return to client
+  const responseError = {
+    statusCode: err.statusCode,
+    message: err.message || StatusCodes[err.statusCode],
+    stack: err.stack,
+  }
+  // console.error(responseError)
+
+  // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn.
+  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+
+  // Return responseError to Front-end
+  res.status(responseError.statusCode).json(responseError)
+}
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
-import express from 'express'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
+  // Create object validation
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict().messages({
       'any.required': 'Title is required',
@@ -30,18 +31,19 @@ const createNew = async (req, res, next) => {
       }),
   })
 
+  //Execute
   try {
-    console.log('req.body: ', req.body)
     //Set boardEarly: false to return all error in once
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-    //next()
-    res
-      .status(StatusCodes.CREATED)
-      .json({ message: 'POST from Validation: API create list boards' })
+    //Navigate to controller layer when complete validate (boardRoute - post)
+    next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message,
-    })
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(
+      StatusCodes.UNPROCESSABLE_ENTITY,
+      errorMessage
+    )
+    next(customError)
   }
 }
 
