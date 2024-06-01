0dc831e3a0e3dac0e442dbe88970a592e9136e8f
Lê Quý Độ <leedo.devwork@gmail.com>
2024-06-02 00:05:00 +0700

    API_GET board details

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModal } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   try {
@@ -9,8 +12,27 @@ const createNew = async (reqBody) => {
     }
 
     //Gọi tới tầng Modal để xử lý lưu bản ghi newBoard vào trong database
+    const createdBoard = await boardModal.createNew(newBoard)
+    console.log(createdBoard)
 
-    return newBoard
+    //Lấy bản ghi board sau khi gọi
+    const getNewBoard = await boardModal.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
+
+    return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModal.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+
+    return board
   } catch (error) {
     throw error
   }
@@ -18,4 +40,5 @@ const createNew = async (reqBody) => {
 
 export const boardService = {
   createNew,
+  getDetails,
 }
